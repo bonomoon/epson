4953e92c051bc001d8933ff0686a7cc35cd21acf
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-20 19:37:05 +0900

    style: 주석 추가

--- app/process/page.js
+++ app/process/page.js
@@ -5,6 +5,7 @@ export default function Process() {
     <div className={styles.container}>
       <div className={styles.progress_msg}>분석 중입니다...</div>
       <div className={styles.progressbar_container}>
+        {/* 값에 따라 길이 수정*/}
         <div className={styles.progressbar}></div>
         <div className={styles.progressbar_bg}></div>
       </div>
