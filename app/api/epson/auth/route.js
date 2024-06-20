edf4a336af590083c32ac4e05d622daa4fb6401b
Bong-oh Moon <bongohmoon@gmail.com>
2024-06-21 04:30:00 +0900

    feat: epson connect authentication

---
+++ app/api/epson/auth/route.js
@@ -0,0 +1,28 @@
+export async function POST(req) {
+  const { email, password } = await req.json();
+  
+  const clientId = process.env.PUBLIC_EPSON_CLIENT_ID;
+  const clientSecret = process.env.PUBLIC_EPSON_CLIENT_SECRET;
+
+  const authHeader = `Basic ${btoa(`${clientId}:${clientSecret}`)}`;
+  
+  const res = await fetch(
+    "https://api.epsonconnect.com/api/1/printing/oauth2/auth/token?subject=printer",
+    {
+      method: "POST",
+      headers: {
+        Authorization: authHeader,
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: new URLSearchParams({
+        grant_type: "password",
+        username: email,
+        password: password,
+      }).toString(),
+    }
+  );
+
+  const data = await res.json();
+  
+  return Response.json(data);
+}
--- app/score/page.js
+++ app/score/page.js
@@ -1,12 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { socket } from "../socket";
-import ScoreHeader from "../../components/ScoreHeader";
+import ScoreHeader from "../../components/score/ScoreHeader";
 import Container from "../../components/Container";
 import ScoreCardSlider from "../../components/score/ScoreCardSlider";
 
 export default function Score() {
+  const epsonAuthRef = useRef();
+
   const [scoreFiles, setScoreFiles] = useState([]);
   const [isConnected, setIsConnected] = useState(false);
   const [transport, setTransport] = useState("N/A");
@@ -52,8 +54,39 @@ export default function Score() {
     setScoreFiles([...scoreFiles, scoreFile]);
   };
 
+  const handleOpenModal = () => {
+    epsonAuthRef.current.showModal();
+  };
+
+  const handleCloseModal = () => {
+    epsonAuthRef.current.close();
+  };
+
+  const handleEpsonConnectAuth = async (event) => {
+    event.preventDefault();
+    
+    const { email } = event.target;
+
+    const response = await fetch("/api/epson/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email.value, password: "" }),
+    });
+      
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log("Authenticated successfully", data);
+      handleCloseModal();
+    } else {
+      console.error("Authentication failed", data);
+    }
+  };
+
   return (
-    <div className="h-full">
+    <div className="h-full ">
       <ScoreHeader className="absolute w-full z-30" />
       <div className="h-full flex flex-col pt-20">
         <Container>
@@ -72,8 +105,8 @@ export default function Score() {
                 </p>
               </div>
               <button
-                id="file-select-btn"
-                className="w-64 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded-lg transition-colors duration-300"
+                className="w-64 bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded-lg transition-colors duration-300"
+                onClick={handleOpenModal}
               >
                 Epson Connect 연결
               </button>
@@ -118,10 +151,7 @@ export default function Score() {
                   className="hidden"
                   onChange={handleFileInputChange}
                 />
-                <button
-                  id="file-select-btn"
-                  className="w-56 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded-lg transition-colors duration-300"
-                >
+                <button className="w-56 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded-lg transition-colors duration-300">
                   변환하기
                 </button>
               </Container>
@@ -129,6 +159,81 @@ export default function Score() {
           )}
         </div>
       </div>
+
+      <dialog
+        ref={epsonAuthRef}
+        className="relative bg-white backdrop:bg-black/20 backdrop:backdrop-blur-sm rounded-lg shadow dark:bg-gray-700"
+        onClick={(event) => {
+          if (event.target === epsonAuthRef.current) {
+            handleCloseModal();
+          }
+        }}
+      >
+        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <h3 className="text-xl font-semibold text-blue-800 dark:text-white">
+            EPSON CONNECT
+          </h3>
+          <button
+            type="button"
+            className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            onClick={handleCloseModal}
+          >
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+        </div>
+        <div className="py-5 px-8">
+          <form className="w-80" onSubmit={handleEpsonConnectAuth}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Epson 제품에 연결된 이메일 ID
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="name@print.epsonconnect.com"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full mt-7 mb-12 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              인증
+            </button>
+            <div className="text-sm font-medium text-center text-gray-500 dark:text-gray-300">
+              미등록 제품인가요?{" "}
+              <a
+                href="https://www.epsonconnect.com/guide/ko/html/p01.htm"
+                target="_blank"
+                className="text-blue-500 hover:underline dark:text-blue-500"
+              >
+                Epson Connect 계정 연동
+              </a>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 }
