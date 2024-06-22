95ae10a880806304323052cba4ae1cc212a8abcf
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-22 17:46:27 +0900

    refactor: 규칙에 맞게 print api 수정

--- app/api/epson/print/[id]/route.js
+++ app/api/epson/print/[id]/route.js
@@ -1,9 +1,12 @@
+import { create } from "@mui/material/styles/createTransitions"
 import { NextResponse } from "next/server"
 
 export async function POST(req, { params }) {
   const authorization = req.headers.get("authorization")
-  const print_mode = "document"
-  const extension = ".pdf"
+  const job_name = "정간보 -> 오선보"
+  const print_mode = "photo"
+  const extension = ".jpg"
+  const content_tpye = print_mode == "photo" ? "image/jpeg" : "application/pdf"
 
   // create print job
   const create_res = await fetch(
@@ -15,7 +18,7 @@ export async function POST(req, { params }) {
         "Content-Type": "application/json; charset=UTF-8",
       },
       body: JSON.stringify({
-        job_name: "sample",
+        job_name: job_name,
         print_mode: print_mode,
         print_setting: {
           media_size: "ms_a4",
@@ -35,7 +38,7 @@ export async function POST(req, { params }) {
   const create_data = await create_res.json()
 
   if (create_res.status != 201) {
-    return Response.json({ error: "Failed to create print job" })
+    return Response.json(create_data)
   }
 
   const job_id = create_data.id
@@ -47,7 +50,7 @@ export async function POST(req, { params }) {
   const upload_res = await fetch(`${upload_url}&File=1${extension}`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/octet-stream",
+      "Content-Type": content_tpye,
       "Content-Length": file.size,
       Authorization: authorization,
     },
@@ -55,7 +58,7 @@ export async function POST(req, { params }) {
   })
 
   if (upload_res.status != 200) {
-    return Response.json({ error: "Failed to upload file" })
+    return Response.json(await upload_res.json(), { status: upload_res.status })
   }
 
   // execute print job
@@ -71,7 +74,9 @@ export async function POST(req, { params }) {
   )
 
   if (execute_res.status != 200) {
-    return Response.json({ error: "Failed to execute print job" })
+    return Response.json(await execute_res.json(), {
+      status: execute_res.status,
+    })
   }
 
   const data = await execute_res.json()
