e5fa6f61212afd816b62e27a07369ce8a72a698d
Bong-oh Moon <bongohmoon@gmail.com>
2024-06-29 03:25:27 +0900

    styles: LoadingSpinner 작았던 문제 해결

--- src/components/score/ProcessView.tsx
+++ src/components/score/ProcessView.tsx
@@ -3,7 +3,7 @@ import LoadingSpinner from "@components/LoadingSpinner";
 export default function ProcessView({ length, progress }) {
   return (
     <>
-      <LoadingSpinner className={"w-10 h-10"} />
+      <LoadingSpinner className={"w-24 h-24"} />
       <div className="">{`${progress}/${length}`}</div>
     </>
   );
